72984783bfe0015514307ac29f03ca8966cf006b
Thales Kenne <thaleskenne@hotmail.com>
2020-04-28 21:22:08 -0300

    Added cryptography to user password

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,10 +1,13 @@
 import User from '../models/User';
+import { hash } from 'bcryptjs';
 import { getRepository } from 'typeorm';
+
 interface Request {
   name: string;
   email: string;
   password: string;
 }
+
 export default class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
     const usersRepository = getRepository(User);
@@ -15,10 +18,12 @@ export default class CreateUserService {
     if (checkUserExists) {
       throw new Error('Email address already used');
     }
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     await usersRepository.save(user);
     return user;
