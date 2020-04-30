128150605cb428ec12afdd71ba915ed18ffbd56c
Thales Kenne <thaleskenne@hotmail.com>
2020-04-30 10:04:40 -0300

    Allowed snake case in eslint

--- backend/src/routes/sessions.routes.ts
+++ backend/src/routes/sessions.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import AuthenticateUserService from '../services/AuthenticateUserService';
+
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
