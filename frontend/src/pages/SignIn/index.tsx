fb45647ba118b2396e1c7db22f7e8fe980b23e6e
SirKadogan <thaleskenne@hotmail.com>
2020-05-18 21:04:03 -0300

    Added logout logic

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -7,7 +7,7 @@ import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
